7c19aa6d52be67626b804201516b1a78550f965b
taze-fullstack <taze@fullstack.ph>
2019-05-02 14:57:26 +0800

    Updates

---
+++ precache-manifest.f905b5a66bde48e24cc4a59913596f49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd4927075ada946b71b4",
+    "url": "/coding-standards/static/js/runtime~main.7349e273.js"
+  },
+  {
+    "revision": "769a3399a93707efe395",
+    "url": "/coding-standards/static/js/main.71975bf0.chunk.js"
+  },
+  {
+    "revision": "4ecaa348be8000a82063",
+    "url": "/coding-standards/static/js/2.c7691db7.chunk.js"
+  },
+  {
+    "revision": "769a3399a93707efe395",
+    "url": "/coding-standards/static/css/main.b09e6cb5.chunk.css"
+  },
+  {
+    "revision": "d09751dc746ec04e1e328b38a85767fa",
+    "url": "/coding-standards/index.html"
+  }
+];
