af6494f7410dad566d904d4d12db47cb23a15289
taze-fullstack <taze@fullstack.ph>
2019-06-04 14:39:53 +0800

    rename title

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ export default class Header extends Component {
   render() {
     return (
       <header>
-        <div>Fullstack HQ - Frontend Guidelines</div>
+        <div>Fullstack HQ - Frontend Handbook</div>
         <div style={{fontSize: '10px'}}>inspired by Code Guide by @mdo</div>
       </header>
     )
